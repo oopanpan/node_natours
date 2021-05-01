2da7c8d6f9ca1a037dddb1cf54175ae2c47173fd
oopanpan <oopanpan@gmail.com>
2021-05-01 09:18:54 -0700

    get request using express

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
+
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'Hello there this is our server', app: 'Natours' });
+// });
+
+// app.post('/', (req, res) => {
+//   res.send('You may post to this endpoint...');
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+//? ROUTE HANDLER
+app.get('/api/v1/tours', (req, res) => {
+  res
+    .status(200)
+    .json({ status: 'success', result: tours.length, data: { tours } });
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
