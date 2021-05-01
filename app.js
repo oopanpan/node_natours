8a88dcdc38b02063ef0c51f4ccd2d3755ace2c84
oopanpan <oopanpan@gmail.com>
2021-05-01 16:06:22 -0700

    post request to main with notes

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require('express');
 
 const app = express();
 
+//! USING MIDDLEWARE for POST REQUEST
+app.use(express.json());
+
 // app.get('/', (req, res) => {
 //   res
 //     .status(200)
@@ -14,17 +17,44 @@ const app = express();
 // });
 
 const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
 //? ROUTE HANDLER
 app.get('/api/v1/tours', (req, res) => {
-  res
-    .status(200)
-    .json({ status: 'success', result: tours.length, data: { tours } });
+    res.status(200).json({
+        status: 'success',
+        result: tours.length,
+        data: { tours },
+    });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+    //   console.log(req.body);
+    const newId = tours[tours.length - 1].id + 1;
+    //? CREATE A NEW OBJECT, INJECTING IDs
+    //* ES5
+    // const newTour = Object.assign({ id: newId }, req.body);
+    //* ES6
+    const newTour = { ...req.body, id: newId };
+    tours.push(newTour);
+    //? use Async
+    fs.writeFile(
+        `${__dirname}/dev-data/data/tours-simple.json`,
+        JSON.stringify(tours),
+        (err) => {
+            //* 200 means OK, 201 means CREATED!!!
+            res.status(201).json({
+                status: 'success',
+                data: {
+                    tour: newTour,
+                },
+            });
+        }
+    );
 });
 
 const port = 3000;
 app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
+    console.log(`App running on port ${port}...`);
 });
