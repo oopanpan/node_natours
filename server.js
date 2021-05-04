4428c7a8c07d09350ee0bde27a6d6ca6a518b705
oopanpan <oopanpan@gmail.com>
2021-05-03 19:00:19 -0700

    begin of mongoose

--- server.js
+++ server.js
@@ -1,7 +1,41 @@
 //! this is gonna be the starting file
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+    '<PASSWORD>',
+    process.env.DATABASE_PASSWORD
+);
+
+mongoose
+    .connect(DB, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+    })
+    .then(() => {
+        console.log('DB connection successful!');
+    });
+
+const tourSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'A tour must have a name'],
+        unique: true,
+    },
+    rating: {
+        type: Number,
+        default: 4.0,
+    },
+    price: {
+        type: Number,
+        required: [true, 'A tour must have a price'],
+    },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
 
 const app = require('./app');
 
