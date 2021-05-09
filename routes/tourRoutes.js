372093c8dbb871d4cf263bd9b95f33b8132bf419
oopanpan <oopanpan@gmail.com>
2021-05-09 02:40:22 -0700

    more aggregation pipeline stages

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -7,6 +7,7 @@ const {
     deleteTour,
     aliasTopTours,
     getTourStats,
+    getMonthlyPlan,
 } = require('../controllers/tourController');
 
 const router = express.Router();
@@ -14,6 +15,7 @@ const router = express.Router();
 //? Custom Route
 router.route('/top-5-value').get(aliasTopTours, getAllTours);
 router.route('/tours-stats').get(getTourStats);
+router.route('/monthly-plan/:year').get(getMonthlyPlan);
 
 router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
