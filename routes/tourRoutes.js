dd83dba70467a0aaaad7ec6169e70c3de1bae132
oopanpan <oopanpan@gmail.com>
2021-05-09 00:28:10 -0700

    alias route

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,9 +5,13 @@ const {
     getTour,
     updateTour,
     deleteTour,
+    aliasTopTours,
 } = require('../controllers/tourController');
 
 const router = express.Router();
+
+//? Custom Route
+router.route('/top-5-value').get(aliasTopTours, getAllTours);
 
 router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
