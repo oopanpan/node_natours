37f1137f2e41ce805eda57901a663bc92450802d
oopanpan <oopanpan@gmail.com>
2021-05-10 01:05:19 -0700

    dev vs prod error disclosure

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,8 +1,35 @@
+const sendErrorDev = (err, res) => {
+    res.status(err.statusCode).json({
+        status: err.status,
+        error: err,
+        message: err.message,
+        stack: err.stack,
+    });
+};
+
+const sendErrorProd = (err, res) => {
+    //? Operational, trusted error: send message to client, attribute was set in the appError class
+    if (err.isOperational) {
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message,
+        });
+        //? Programming or other unkown error: don't leak error details
+    } else {
+        console.error('Error : ', err);
+        res.status(500).json({
+            status: 'error',
+            message: 'Internal Server Error',
+        });
+    }
+};
+
 module.exports = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'error';
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message,
-    });
+    if (process.env.NODE_ENV === 'development') {
+        sendErrorDev(err, res);
+    } else if (process.env.NODE_ENV === 'production') {
+        sendErrorProd(err, res);
+    }
 };
