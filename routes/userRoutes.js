044c730f1c8ee4f7a477065473cfe061c5a55aa2
oopanpan <oopanpan@gmail.com>
2021-05-14 17:46:05 -0700

    sign up auth route, controller created

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,8 +7,11 @@ const {
     updateUser,
     deleteUser,
 } = require('../controllers/userController');
+const { signup } = require('../controllers/authController');
+
 const router = express.Router();
 
+router.route('/signup').post(signup);
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
