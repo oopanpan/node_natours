e39e0b11b88fbd26a1322c23f4db5ee3af42a01e
oopanpan <oopanpan@gmail.com>
2021-06-06 00:02:52 -0700

    unhandled rejection complete

--- server.js
+++ server.js
@@ -23,12 +23,12 @@ mongoose
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`App running on port ${port}...`);
 });
 
 process.on('unhandledRejection', (err) => {
     console.log(err.name, err.message);
     console.log('UNHANDLED REJECTION, shutting down...');
-    process.getMaxListeners(1);
+    server.close(() => process.exit(1));
 });
